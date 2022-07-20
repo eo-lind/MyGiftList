1b24d3367e678bb3c815eef787db27480a700ca5
Olivia Lind <eo.lind@gmail.com>
2022-07-20 15:51:40 -0500

    created a DbUtils file with helper methods

---
+++ MyGiftList/Utils/DBUtils.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace MyGiftList.Utils
+{
+    // helper functions
+    public static class DbUtils
+    {
+        // will get a string from data being read (will return the string or null)
+        public static string GetString(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+
+            return reader.GetString(ordinal);
+        }
+
+        // will get an int from data being read (will return the number or null)
+        public static int GetInt(SqlDataReader reader, string column)
+        {
+            return reader.GetInt32(reader.GetOrdinal(column));
+        }
+
+        // will get/return a date from data being read
+        public static DateTime GetDateTime(SqlDataReader reader, string column)
+        {
+            return reader.GetDateTime(reader.GetOrdinal(column));
+        }
+
+        // will get an int from data being read, check to see if the column value is null (returns the number or null)
+        public static int? GetNullableInt(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+
+            return reader.GetInt32(ordinal);
+        }
+
+        // will get a date from data being read, check to see if the column value is null (returns a date or null)
+        public static DateTime? GetNullableDateTime(SqlDataReader reader, string column)
+        {
+            var ordinal = reader.GetOrdinal(column);
+            if (reader.IsDBNull(ordinal))
+            {
+                return null;
+            }
+
+            return reader.GetDateTime(ordinal);
+        }
+
+        // checks whether column value is null
+        public static bool IsDbNull(SqlDataReader reader, string column)
+        {
+            return reader.IsDBNull(reader.GetOrdinal(column));
+        }
+
+        // checks whether column value is not null (using the previous method)
+        public static bool IsNotDbNull(SqlDataReader reader, string column)
+        {
+            return !IsDbNull(reader, column);
+        }
+
+        // adds a parameter to a SqlCommand if it's not null
+        public static void AddParameter(SqlCommand cmd, string name, object value)
+        {
+            if (value == null)
+            {
+                cmd.Parameters.AddWithValue(name, DBNull.Value);
+            }
+            else
+            {
+                cmd.Parameters.AddWithValue(name, value);
+            }
+        }
+    }
+}
