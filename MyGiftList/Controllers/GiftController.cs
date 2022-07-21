e273579dc0fb56f3343f8b0ff23afde9c5ebdfa3
Olivia Lind <eo.lind@gmail.com>
2022-07-21 10:14:33 -0500

    viewing all gifts works on server side

---
+++ MyGiftList/Controllers/GiftController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGiftList.Repositories;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyGiftList.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GiftController : ControllerBase
+    {
+        private readonly IGiftRepository _giftRepository;
+        public GiftController(IGiftRepository giftRepository)
+        {
+            _giftRepository = giftRepository;
+        }
+
+        // GET: api/<GiftController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_giftRepository.GetAll());
+        }
+
+        //// GET api/<GiftController>/5
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST api/<GiftController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<GiftController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<GiftController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
