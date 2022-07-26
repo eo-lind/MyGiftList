710af740a1aaeb14ac970db974d3a5a5a58ea9ca
Olivia Lind <eo.lind@gmail.com>
2022-07-26 11:11:49 -0500

    removed commented out code

--- MyGiftList/client/src/components/RecipientDetails.js
+++ MyGiftList/client/src/components/RecipientDetails.js
@@ -58,56 +58,6 @@ const RecipientDetails = () => {
           ))}
       </div>
   )
-
-  // return (
-  //     <div className="container">
-  //         <div className="row justify-content-center">
-  //             <h4>{recipient.name}</h4>
-  //             <p>
-  //                 <strong></strong>
-  //             </p>
-  //             <div>
-  //                 {recipient.recipientGifts?.map((recipientGift) => (
-  //                     <div key={"recipientGift__" + recipientGift.id}>
-  //                         <p>
-  //                             <strong>Birthday:</strong> {recipient.birthday}
-  //                         </p>
-  //                         <hr />
-  //                             <h5>Gifts for {recipient.name}:</h5>
-  //                         {recipient.recipientGifts?.map((singleGift) => (
-  //                             <Card>
-  //                                 <CardBody>
-  //                                     <div className="gift-image-container">
-  //                                         <img
-  //                                             src={singleGift.gift.imageUrl}
-  //                                             alt="image of the gift"
-  //                                             className="gift-image"
-  //                                         />
-  //                                     </div>
-  //                                     <h6 key={singleGift.id}>
-  //                                         {singleGift.gift.name}
-  //                                     </h6>
-  //                                     <p>${singleGift.gift.price}</p>
-  //                                     <p>
-  //                                         Available{" "}
-  //                                         <a
-  //                                             href={singleGift.gift.shopUrl}
-  //                                             target="_blank"
-  //                                         >
-  //                                             here
-  //                                         </a>
-  //                                     </p>
-  //                                     <p>Quantity: {singleGift.qty}</p>
-  //                                     <p>Notes: {singleGift.notes}</p>
-  //                                 </CardBody>
-  //                             </Card>
-  //                         ))}
-  //                     </div>
-  //                 ))}
-  //             </div>
-  //         </div>
-  //     </div>
-  // )
 }
 
 export default RecipientDetails
