87d7003d2f16cfc94a5fd0587fe0a39375e29d94
Olivia Lind <eo.lind@gmail.com>
2022-07-27 23:35:22 -0500

    removed some unused code

--- MyGiftList/Controllers/GiftController.cs
+++ MyGiftList/Controllers/GiftController.cs
@@ -4,7 +4,6 @@ using MyGiftList.Models;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace MyGiftList.Controllers
 {
