f559f65065125879acbab41fcf61022d2d56736e
Olivia Lind <eo.lind@gmail.com>
2022-07-28 11:08:17 -0500

    added a comment and removed unused code

--- MyGiftList/client/src/components/RecipientGiftEditForm.js
+++ MyGiftList/client/src/components/RecipientGiftEditForm.js
@@ -4,68 +4,71 @@ import { getRecipientGiftById, updateRecipientGift } from "../modules/recipientG
 import { Button, Form, FormGroup, Label, Input } from "reactstrap"
 
 const RecipientGiftEditForm = () => {
-  const [recipientGift, setRecipientGift] = useState({})
-  const navigate = useNavigate()
-  const { recipientGiftId } = useParams()
+    const [recipientGift, setRecipientGift] = useState({})
+    const navigate = useNavigate()
+    const { recipientGiftId } = useParams()
 
-  useEffect(() => {
-      getRecipientGiftById(recipientGiftId).then((resp) => setRecipientGift(resp))
-  }, [])
+    useEffect(() => {
+        getRecipientGiftById(recipientGiftId).then((resp) =>
+            setRecipientGift(resp)
+        )
+    }, [])
 
-  const handleInputChange = (evt) => {
-      let target = { ...recipientGift }
+    const handleInputChange = (evt) => {
+        let target = { ...recipientGift }
 
-      target[evt.target.id] = evt.target.value
-      setRecipientGift(target)
-      // const value = evt.target.value
-      // const key = evt.target.id
-      // const recipientGiftCopy = { ...recipientGift }
-      // recipientGiftCopy[key] = value
+        target[evt.target.id] = evt.target.value
+        setRecipientGift(target)
+    }
 
-      // setRecipientGift(recipientGiftCopy)
-  }
+    const handleClickSaveRecipientGift = () => {
+        updateRecipientGift(recipientGift).then(
+            navigate(`/recipients/${recipientGift.recipientId}`)
+        )
+    }
 
-  const handleClickSaveRecipientGift = () => {
-      updateRecipientGift(recipientGift).then(navigate(`/recipients/${recipientGift.recipientId}`))
-}
+    // https://thewebdev.info/2021/09/18/how-to-fix-the-a-component-is-changing-an-uncontrolled-input-of-type-text-to-be-controlled-error-in-react/ (used to resolve controlled input issue)
 
+    return (
+        <Form>
+            <h4>Edit {recipientGift.id}</h4>
+            <FormGroup>
+                <Label for="quantity">Quantity:</Label>
+                <select
+                    name="qty"
+                    id="qty"
+                    value={recipientGift.qty ?? ""}
+                    onChange={handleInputChange}
+                >
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+            </FormGroup>
 
-  return (
-      <Form>
-          <h4>Edit {recipientGift.id}</h4>
-          <FormGroup>
-              <Label for="quantity">Quantity:</Label>
-              <select name="qty" id="qty" value={recipientGift.qty ?? ""} onChange={handleInputChange}>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                  <option value="4">4</option>
-                  <option value="5">5</option>
-                  <option value="6">6</option>
-                  <option value="7">7</option>
-                  <option value="8">8</option>
-                  <option value="9">9</option>
-                  <option value="10">10</option>
-              </select>
-          </FormGroup>
-
-          <FormGroup>
-              <Label for="notes">Notes:</Label>
-              <Input
-                  type="text"
-                  name="notes"
-                  id="notes"
-                  placeholder="Notes (size, color, etc.)"
-                  value={recipientGift.notes ?? ""}
-                  onChange={handleInputChange}
-              />
-          </FormGroup>
-          <Button id="submit" onClick={handleClickSaveRecipientGift}>
-              Update
-          </Button>
-      </Form>
-  )
-
+            <FormGroup>
+                <Label for="notes">Notes:</Label>
+                <Input
+                    type="text"
+                    name="notes"
+                    id="notes"
+                    placeholder="Notes (size, color, etc.)"
+                    value={recipientGift.notes ?? ""}
+                    onChange={handleInputChange}
+                />
+            </FormGroup>
+            <Button id="submit" onClick={handleClickSaveRecipientGift}>
+                Update
+            </Button>
+        </Form>
+    )
 }
 
 export default RecipientGiftEditForm
