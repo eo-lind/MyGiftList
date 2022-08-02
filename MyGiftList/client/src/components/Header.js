846773bfbed4b830ac6d0cf181ef35f0f308a5e5
Olivia Lind <eo.lind@gmail.com>
2022-08-02 10:05:08 -0500

    deleted unused code from header

--- MyGiftList/client/src/components/Header.js
+++ MyGiftList/client/src/components/Header.js
@@ -4,53 +4,6 @@ import { logout } from "../modules/authManager";
 
 const Header = ({ isLoggedIn }) => {
   return (
-      // <nav className="navbar navbar-expand navbar-dark bg-info">
-      //     <Link to="/" className="navbar-brand">
-      //         My Gift List
-      //     </Link>
-
-      //     <ul className="navbar-nav mr-auto">
-      //         {isLoggedIn && (
-      //             <>
-      //                 <li className="nav-item">
-      //                     <Link to="/gifts" className="nav-link">
-      //                         Gifts
-      //                     </Link>
-      //                 </li>
-      //                 <li className="nav-item">
-      //                     <Link to="/recipients" className="nav-link">
-      //                         Recipients
-      //                     </Link>
-      //                 </li>
-      //                 <li className="nav-item">
-      //                     <p
-      //                         className="nav-link"
-      //                         style={{ cursor: "pointer" }}
-      //                         onClick={logout}
-      //                     >
-      //                         Logout
-      //                     </p>
-      //                 </li>
-      //             </>
-      //         )}
-
-      //         {!isLoggedIn && (
-      //             <>
-      //                 <li>
-      //                     <Link to="/login" className="nav-link">
-      //                         Login
-      //                     </Link>
-      //                 </li>
-      //                 <li>
-      //                     <Link to="/register" className="nav-link">
-      //                         Register
-      //                     </Link>
-      //                 </li>
-      //             </>
-      //         )}
-      //     </ul>
-      // </nav>
-
       <nav className="nav-main">
           <Link to="/" className="navbar-title">
               My Gift List
